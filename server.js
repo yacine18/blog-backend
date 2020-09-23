6b7f639583440757a26f73dc7248f3f3409d0e48
yacine18 <hilali.yacin@gmail.com>
2020-09-23 16:53:53 +0100

    blog api

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 
-// app.use( (req,res,next)=> {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization')
+app.use( (req,res,next)=> {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization')
 
-//     if(req.method === 'OPTIONS'){
-//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET')
-//         return res.status(200).json({})
-//     }
-// })
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET, OPTIONS')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 //routes
 const articlesRouter = require('./routes/articles.router')
