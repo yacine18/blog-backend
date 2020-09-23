4289c4a207531e407cab3015b5922e81b2451847
yacine18 <hilali.yacin@gmail.com>
2020-09-23 16:58:49 +0100

    blog api

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use('/uploads', express.static('uploads'))
 
 app.use( (req,res,next)=> {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    // res.header('Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization')
 
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET, OPTIONS')
-        return res.status(200).json({})
-    }
+    // if(req.method === 'OPTIONS'){
+    //     res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET, OPTIONS')
+    //     return res.status(200).json({})
+    // }
     next()
 })
 
